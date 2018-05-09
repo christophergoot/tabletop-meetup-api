c8e76fb855751b17273908fd5c2c123d8c82f7dc
christophergoot <christophergoot@gmail.com>
2018-05-09 10:12:57 -0700

    setup routers

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
 const PORT = process.env.PORT || 3030;
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
+const { router: usersRouter } = require('./routers/users');
+const { router: eventsRouter } = require('./routers/events');
+app.use('/api/users/', usersRouter);
+app.use('/api/events/', eventsRouter);
+
+
+app.get('/api/', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'you found the API router'
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
