922c9cc745416af5133462ccb59aa424f0b12413
christophergoot <christophergoot@gmail.com>
2018-05-30 23:30:17 -0700

    updated pageCount method

--- routers/collections.js
+++ routers/collections.js
@@ -23,13 +23,34 @@ function sortGamesByMethod(games, sort) {
 	return sorted;
 }
 
+const filter = [
+	{
+		field: 'yearPublished',
+		is: '$lte',
+		value: 2015,
+		and: '$gt',
+		andValue: 1985
+	},
+	{
+		field: 'rsvp',
+		is: '$eq',
+		value: 'attending'
+	}
+];
+const filterQuery = { $match: {} };
+filter.forEach(filter => {
+	if (filter.and)
+		filterQuery.$match[filter.field] = { [filter.is]: filter.value, [filter.and]: filter.andValue };
+	else filterQuery.$match[filter.field] = { [filter.is]: filter.value };
+});
+
 async function getCollection(req) {
 	let userId = '';
 	if (req.params.userId) userId = req.params.userId;
 	else userId = req.user.userId;
 	const { filter, sortMethod, sortDirection } = req.query;
 
-	const limit = parseInt(req.query.limit) || 25;
+	const limit = (parseInt(req.query.limit)) || 25;
 	const page = parseInt(req.query.page) || 1;
 	const skip = (page -1) * limit;
 	
@@ -42,21 +63,38 @@ async function getCollection(req) {
 	};
 	const sortQuery = {};
 	sortQuery[`games.${sort.method}`] = parseInt(sort.direction);
-	// const sortQuery = { 'games.name': -1 };
-	const pageCount = await Collection.findOne({userId})
-		.then(c => Math.ceil(c.games.length / limit));
+	
+	let pageCount = 0;
+	let gameList;
+
+	if (req.query.limit === '0') {
+		gameList = await Collection
+			.aggregate( [
+				{ $match: { userId } },
+				{ $unwind: '$games' },
+				// { $match: {'games.owned': true }},
+				// { $match: filterQuery },
+				{ $sort: sortQuery },
+				// { $sort: JSON.stringify(sortQuery) },
+				{ $skip: skip }
+			] );
+	} else {
+		pageCount = await Collection.findOne({userId})
+			.then(c => Math.ceil(c.games.length / limit));
+
+		gameList = await Collection
+			.aggregate( [
+				{ $match: { userId } },
+				{ $unwind: '$games' },
+				// { $match: {'games.owned': true }},
+				// { $match: filterQuery },
+				{ $sort: sortQuery },
+				// { $sort: JSON.stringify(sortQuery) },
+				{ $skip: skip },
+				{ $limit: limit }
+			] );
+	}
 		
-	const gameList = await Collection
-		.aggregate( [
-			{ $match: { userId } },
-			{ $unwind: '$games' },
-			// { $match: {'games.owned': true }},
-			// { $match: filterQuery },
-			{ $sort: sortQuery },
-			// { $sort: JSON.stringify(sortQuery) },
-			{ $skip: skip },
-			{ $limit: limit }
-		] );
 	const games = [];
 	gameList.forEach(game => games.push(game.games));
 	const collection = { userId, sort, limit, page, pageCount, games };
--- routers/events.js
+++ routers/events.js
@@ -30,56 +30,84 @@ function retrieveGameList(userId) {
 		});
 }
 
-async function attachGameList(event) {
+async function attachGameList(event,limit,skip,sort,filter) {
 	const userIds = event.guests.map(guest => guest.userId);
+	const sortQuery = {};
+	sortQuery[`games.${sort.method}`] = parseInt(sort.direction);
+
 	const gameList = await Collection.aggregate( [
 		{ $match: { userId: {$in: userIds } } },
 		{ $unwind: '$games' },
 		{ $match: { 'games.owned': true } },
-		{ $sort: { 'games.name': 1 } },
-		// { $sort: { field: 'games.name', test: -1 } },
-		{ $skip: 0 },
-		// { $limit: 5 },
+		{ $sort: sortQuery },
 	] );
-	const eventCopy = JSON.parse(JSON.stringify(event));
-	eventCopy.games = {
-		...event.games,
-		list: gameList
-	};
-	return eventCopy;
-}
-
-
-function HACKEDattachGameList(event) {
-	const gameList = [];
-	const guestList = [];
-	event.guests.forEach(guest => guestList.push(guest.userId));
-	return Promise.all(guestList.map(retrieveGameList))
-		.then(gameLists => {
-			gameLists.forEach(list => {
-				list.forEach(game => gameList.push(game));
-			});
-			const eventCopy = JSON.parse(JSON.stringify(event));
-			eventCopy.games = {
-				...event.games,
-				list: gameList
-			};
-			return eventCopy;
+	const games = [];
+	gameList.forEach(game => {
+		const existingGameIndex = games.findIndex(g => g.gameId === game.games.gameId);
+		if (existingGameIndex >= 0) games[existingGameIndex].owners.push(game.userId);
+		else games.push({
+			gameId: game.games.gameId,
+			name: game.games.name,
+			image: game.games.image,
+			thumbnail: game.games.thumbnail,
+			minPlayers: game.games.minPlayers,
+			maxPlayers: game.games.maxPlayers,
+			playingTime: game.games.playingTime,
+			yearPublished: game.games.yearPublished,
+			bggRating: game.games.bggRating,
+			averageRating: game.games.averageRating,
+			rank: game.games.rank,
+			isExpansion: game.games.isExpansion,
+			owners: [{userId: game.userId}]
 		});
+
+	});
+	const page = (skip / limit) + 1;
+	const pageCount = Math.ceil(games.length / limit);
+	if (limit === 0) skip = 0;
+	games.splice(0,skip);
+	if (limit) games.splice(limit);
+
+	const eventCopy = JSON.parse(JSON.stringify(event));
+	
+	// update the eventCopy with preexisting fields
+	[	{	field: 'limit', value: limit },
+		{	field: 'skip', value: skip },
+		{	field: 'page', value: page },
+		{	field: 'pageCount', value: pageCount },
+		{	field: 'sort', value: sort },
+		{	field: 'filter', value: filter },
+		{	field: 'games', value: games },
+	].forEach(el => eventCopy[el.field] = el.value);
+	
+	return eventCopy;
 }
 
 router.get('/:eventId', (req, res) => {
 	const { eventId } = req.params;
 	const { userId } = req.user;
+	const { filter, sortMethod, sortDirection } = req.query;
+	const sort = {
+		method: sortMethod || 'name',
+		direction: sortDirection || 1
+	};
+	const limit = parseInt(req.query.limit) || 25;
+	const page = parseInt(req.query.page) || 1;
+	const skip = (page -1) * limit;
+
 	return Event
 		.findOne({'_id':eventId})
 		// .populate('guests.user', {firstName,lastName,username})
 		// .populate('guests.user', ['firstName','lastName','username'])
 		.populate('guests.user', 'firstName lastName username')
 		.then(event => event.serialize())
-		.then(event => attachGameList(event))
+		.then(event => attachGameList(event,limit,skip,sort,filter))
 		.then(event => res.json(event));
 });
+
+const addGames = event => {
+	return attachGameList(event,25,0,{method:'name',direction:1},'');
+};
 
 router.get('/', (req, res) => {
 	const { userId } = req.user;
@@ -88,12 +116,23 @@ router.get('/', (req, res) => {
 		.populate('guests.user', 'firstName lastName username')
 		.then(events => events.map(event => event.serialize()))
 
-		.then(events => { // attach combined gamelist to event
-			return Promise.all(events.map(async event => {
-				const eventWithGames = await attachGameList(event);
-				return eventWithGames;
-			}));
+		.then(async events => {
+			const many = await Promise.all(events.map(addGames));
+			console.log(many);
+			// events.map(async event => {
+			// 	const newEvent = await attachGameList(event,25,0,{method:'name',direction:1},'');
+			// 	return newEvent;
+			// });
+			return many;
 		})
+
+
+		// .then(events => { // attach combined gamelist to event
+		// 	return Promise.all(events.map(async event => {
+		// 		const eventWithGames = await attachGameList(event);
+		// 		return eventWithGames;
+		// 	}));
+		// })
 		.then(events => res.json(events));
 });
 
